13f26518ea563d5f805eb4025ea374f254318f88
Francesco-Maisto <franco.maisto@outlook.com>
2023-10-12 16:55:55 +0200

    esercizio-g4

---
+++ m1/s3/g4/js/script.js
@@ -0,0 +1,18 @@
+let tabellone = document.querySelector(".tabellone")
+let estrazione = document.querySelector(".estrazione")
+let numEstratto = document.querySelector(".numeroEstratto")
+let numero = document.querySelector(".numero")
+for (let i = 1; i <=76; i++){
+    let numero = document.createElement("div")
+    numero.classList.add("numero")
+    numero.innerText= i
+    tabellone.append(numero)
+    
+}
+
+estrazione.addEventListener("click",function() {
+    let estratto =Math.floor(Math.random() * 77);
+    numEstratto.innerText= estratto
+   
+})
+
